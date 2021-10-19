dcdc5587f488a1cb5cb91c3331f24b976a0e3a80
uraankhayayaal <uraankhayayaal@gmail.com>
2021-10-19 13:11:03 +0300

    fix migration for add role

--- src/migrations/m200623_004419_add_role.php
+++ src/migrations/m200623_004419_add_role.php
@@ -12,15 +12,16 @@ class m200623_004419_add_role extends Migration
         $auth = Yii::$app->authManager;
 
         $settingRedactor = $auth->getPermission('settings');
-        if($settingRedactor != null || $settingRedactor != false){
+        if($settingRedactor == null){
             $settingRedactor = $auth->createPermission('settings');
             $settingRedactor->description = 'Редактирование настроек';
+            
             $auth->add($settingRedactor);
+        
+            $operator = $auth->getRole('operator');
+            if($operator != null || $operator != false)
+                $auth->addChild($operator,$settingRedactor);
         }
-
-        $operator = $auth->getRole('operator');
-        if($operator != null || $operator != false)
-            $auth->addChild($operator,$settingRedactor);
     }
 
     public function safeDown()
